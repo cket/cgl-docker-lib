e21b24047910d829fe644b18db16e99437f9ac4a
Christopher 'CJ' Ketchum <cketchum@ucsc.edu>
2016-12-12 10:21:36 -0800

    Update & fix test (connected to #211)

--- rnaseq-cgl-pipeline/test.py
+++ rnaseq-cgl-pipeline/test.py
@@ -8,7 +8,7 @@ class TestRNASeqPipeline(unittest.TestCase):
 
     def test_docker_call(self):
         # print sys.argv
-        tool = ['quay.io/ucsc_cgl/rnaseq-cgl-pipeline:1.9.1--{}'.format(tag)]
+        tool = ['quay.io/ucsc_cgl/rnaseq-cgl-pipeline:{}'.format(tag)]
         base = ['docker', 'run']
         args = ['--star=/foo', '--rsem=/foo', '--kallisto=/foo', '--samples=/foo']
         sock = ['-v', '/var/run/docker.sock:/var/run/docker.sock']
@@ -16,23 +16,14 @@ class TestRNASeqPipeline(unittest.TestCase):
         sample = ['-v', '/bar:/samples']
         inputs = ['-v', '/foobar:/inputs']
         # Check base call for help menu
-        out = check_docker_output(command=base + tool, assert_1=False)
+        out = check_docker_output(command=base + tool, assert_fail=False)
         self.assertTrue('Please see the complete documentation' in out)
-        self.assertFalse('foo bar' in out)
-        # Check for required mirror mounts
-        self.assertTrue('Wrong number of mirror mounts' in check_docker_output(base + sock + tool + args))
-        # Check for mirror mount when input/sample mount is used
-        self.assertTrue('Wrong number of mirror mounts' in
-                        check_docker_output(base + sock + sample + inputs + tool + args))
-        # Check for more than one mirror mount
-        self.assertTrue('Wrong number of mirror mounts' in check_docker_output(
-            base + sock + mirror + ['-v', '/bar:/bar'] + tool + args))
 
 
-def check_docker_output(command, assert_1=True):
+def check_docker_output(command, assert_fail=True):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = process.communicate()
-    if assert_1:
+    if assert_fail:
         assert process.returncode == 1
     else:
         assert process.returncode == 0
